30e8d8c919135000ccb8a9982e06cd8a6f6bd6e8
Zalary Young <338654+zalary@users.noreply.github.com>
2024-06-12 16:01:58 +0000

    Target environment fetch updates

---
+++ envs/Netsuite-Prod/static-resources/netsuite/FileCabinet/SuiteScripts/InnerFolder/e2eTest.js
@@ -0,0 +1,12 @@
+/**
+ * @NApiVersion 2.X
+ * @NScriptType UserEventScript
+ */
+define(["N/log", "N/https"], function(log, https) {
+  function beforeLoad() {}
+  
+  return {
+    beforeLoad: beforeLoad
+    //afterSubmit
+  };
+});
