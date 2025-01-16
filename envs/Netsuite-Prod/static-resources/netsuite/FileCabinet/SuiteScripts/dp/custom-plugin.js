a580c9f2d4fbcc111c80bc816c06aa9ce4e34e0e
Zalary Young <338654+zalary@users.noreply.github.com>
2025-01-16 22:11:11 +0000

    Fetch: 25 elements changed
    Type: Full Fetch
    Summary: https://app.salto.io/goto?target=fetch&orgId=bb6cb5ce-46ed-4afe-918e-35bf269875f5&envId=84ea1863-76df-446c-bc2f-c2edf136c84a&operationId=415f160c-d81b-4b32-95c9-1e7a368dcb10
    Initiated by: Zalary Young
    
    25 elements were changed in NetSuite, of the following types:
     - 1 change in savedsearch
     - 21 changes in transactionForm
     - 1 change in workflow
     - 2 changes in other types

---
+++ envs/Netsuite-Prod/static-resources/netsuite/FileCabinet/SuiteScripts/dp/custom-plugin.js
@@ -0,0 +1,13 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType plugintypeimpl
+ */
+define(function() {
+    return {
+        doTheMagic: function(operand1, operand2) {
+            return operand1 + operand2;
+        }
+    }
+}); 
+
+        
