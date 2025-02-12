b878f37a433a1f75ff828390aacd4982b47d446f
Salto Scheduled Fetch <scheduled-fetch@salto.io>
2025-02-12 06:38:34 +0000

    Fetch: `gwr1.js` modified
    Type: Full Fetch
    Summary: https://app.salto.io/goto?target=fetch&orgId=bb6cb5ce-46ed-4afe-918e-35bf269875f5&envId=84ea1863-76df-446c-bc2f-c2edf136c84a&operationId=63b7a792-86d7-412a-85e4-bf674b3251df
    Initiated by: Scheduled Fetch
    
    1 element was changed in NetSuite

--- envs/Netsuite-Prod/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr1.js
+++ envs/Netsuite-Prod/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr1.js
@@ -11,7 +11,13 @@ define(['N/record', 'N/search'],
 
  */
 function(record, search) {
+
     // GR 20240313-123224
+
+    
+    // GR 241203 
+
+
     // GR 20240503-132414
 
     // GR 20240723-095848
@@ -26,11 +32,14 @@ function(record, search) {
      * @since 2015.2
      */
     function pageInit(scriptContext) {
+
         const s = "20240313-122646"
-        alert("Bonjour tout la monde")
+        alert("Hello world")
+
 
     }
     return {
+        // non-conflicting change 20250211-101911
         pageInit: pageInit,
     };
-});
+})
